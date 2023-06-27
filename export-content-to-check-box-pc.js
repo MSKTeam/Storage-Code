1ef406e06aef7adb210ce83ca60d60e82740ce9d
MSKTeam <126191199+MSKTeam@users.noreply.github.com>
2023-06-28 00:30:30 +0700

    Create export-content-to-check-box-pc.js

---
+++ export-content-to-check-box-pc.js
@@ -0,0 +1,20 @@
+window.addEventListener('load',function(){
+document.getElementsByClassName("check-box")[0].innerHTML = content;
+const imgTags = document.querySelectorAll(".check-box img");
+if (imgTags.length > 0) {
+  imgTags.forEach((imgTag) => {
+    if (imgTag.hasAttribute("src")) {
+      const domains = ["i0.wp.com", "i1.wp.com", "i2.wp.com", "i3.wp.com"];
+      const randomDomain = domains[Math.floor(Math.random() * domains.length)];
+      const originalSrc = imgTag.getAttribute("src");
+      const newSrc = originalSrc.replace(/^https?:\/\//i, '');
+      const finalSrc = `https://${randomDomain}/${newSrc}`;
+      imgTag.setAttribute("src", finalSrc);
+      imgTag.setAttribute("data-src", finalSrc);
+      imgTag.removeAttribute("src");
+      imgTag.classList.add("lazyload");
+      imgTag.setAttribute("data-src", imgTag.getAttribute("data-src").replace("s1600", "s1600-rw"));
+     }
+   });
+  }
+});
