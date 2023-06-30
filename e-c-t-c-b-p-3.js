36e1ac51f2434596c84e6b2afefc5f7f5e346303
MSKTeam <126191199+MSKTeam@users.noreply.github.com>
2023-06-30 14:33:30 +0700

    Update e-c-t-c-b-p-3.js

--- e-c-t-c-b-p-3.js
+++ e-c-t-c-b-p-3.js
@@ -2,14 +2,13 @@ window.addEventListener('load', function() {
   document.getElementsByClassName("check-box")[0].innerHTML = content;
   const imgTags = document.querySelectorAll(".check-box img");
   if (imgTags.length > 0) {
-    imgTags.forEach((imgTag) => {
+    imgTags.forEach((imgTag, index) => {
       if (imgTag.hasAttribute("src")) {
         const domains = ["i0.wp.com", "i1.wp.com", "i2.wp.com", "i3.wp.com"];
         const randomDomain = domains[Math.floor(Math.random() * domains.length)];
         const originalSrc = imgTag.getAttribute("src");
         const newSrc = originalSrc.replace(/^https?:\/\//i, '');
         const finalSrc = `https://${randomDomain}/${newSrc}`;
-        imgTag.setAttribute("src", finalSrc);
         imgTag.setAttribute("data-src", finalSrc);
         imgTag.removeAttribute("src");
         imgTag.removeAttribute("title");
@@ -22,7 +21,9 @@ window.addEventListener('load', function() {
         linkTag.as = "image";
         linkTag.setAttribute("fetchpriority", "low");
         linkTag.href = finalSrc.replace("s1600", "s1600-rw");
-        document.head.appendChild(linkTag);
+        setTimeout(() => {
+          document.head.appendChild(linkTag);
+        }, index * 200);
       }
     });
   }
