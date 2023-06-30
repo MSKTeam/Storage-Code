22e8bea84fc18205f67651b2f9ba15a4e323cf54
MSKTeam <126191199+MSKTeam@users.noreply.github.com>
2023-07-01 02:42:12 +0700

    Update and rename id-all-page-2.js to id-all-page-3.js

--- id-all-page-3.js
+++ id-all-page-3.js
@@ -1,9 +1,9 @@
+window.addEventListener('load',function(){
 var mybutton=document.getElementById("scTop");window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>150||document.documentElement.scrollTop>150){mybutton.style.display="block"}else{mybutton.style.display="none"}}function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0};
 function myMenu(){var a=document.getElementById("PageList1");a.classList.toggle("shwx")};
 function fungsiSearch(){var a=document.getElementById("Query-input");a.classList.toggle("mystyle")};
 function darkMode(){localStorage.setItem("mode","darkmode"===localStorage.getItem("mode")?"light":"darkmode"),"darkmode"===localStorage.getItem("mode")?document.querySelector("#modeSwitch").classList.add("dark-mode"):document.querySelector("#modeSwitch").classList.remove("dark-mode")};
 function copyFunction(){document.getElementById("getlink").select(),document.execCommand("copy"),document.getElementById("share-notif").innerHTML="<span>Link copied!</span>" };
-window.addEventListener('load',function(){
   var link1 = document.createElement('link');
   link1.href = '/feeds/posts/default/-/Project?orderby=published&amp;alt=json-in-script&amp;max-results=150&amp;callback=update.compile';
   link1.rel = 'prefetch';
